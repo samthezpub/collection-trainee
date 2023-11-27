c5e3a0f20532c5802ef8ddc9f300643742a155e9
Vladimir <lil.rhymes9@gmail.com>
2023-11-28 00:59:52 +0300

    LinkedList Theory

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Long from = System.currentTimeMillis();
+
+        /*
+         *THEORY
+         * LinkedList отличается от ArrayList тем, что у ArrayList'a в основе лежит массив
+         * А у LinkedList - контейнеры, в которых находятся данные
+         * При добавлении элемента в ArrayList, если его capacity недостаточно создаётся новый
+         * В то время как LinkedList просто добавляет в конец ещё один контейнер
+         * В LinkedList элементы связаны последовательно
+         * В LinkedList сложность алгоритма O(1) для вставки, удаления. O(n) для получения
+         * В ArrayList сложность алгоритма O(n) для вставки, удаления. O(1) для получения
+         * Таким образом можно понять, что ArrayList через n-ое количество операций пересоздаётся,
+         * что негативно сказывается на производительности
+         * LinkedList не пересоздаётся, но получение становится для него тяжкой задачей
+         * Пример хорошего использования LinkedList на мой взгляд - логирование ходов шахматной игры
+         * В этом примере в постоянно добавляюется история, а получение может не происходить вовсе,
+         * если реализовать это таким образом, чтобы сразу после совершения хода
+         * это отрисовывалось на ui или в браузере, сам ход, а не последний полученный ход из списка
+         *
+        */
+
+//        HashMap<Integer, User> userHashMap = new HashMap<>();
+//        userHashMap.put(0, new User("Oleg", 10, 100));
+//        userHashMap.put(1, new User("Vasiliy", 12, 1002));
+//        userHashMap.put(2, new User("Kira", 14, 1003));
+//        userHashMap.put(null, new User("Dadya", 41, 20000));
+//
+//        userHashMap.forEach((integer, user) -> System.out.println("" +integer + user));
+
+//        HashMap<Integer,User> userHashMap = new HashMap<>();
+//        User user = new User("Володя", 10, 2500);
+//        User userSecond = new User("Володя", 10, 2500);
+//
+//        for (int i = 0; i < 1000000000; i++) {
+//            boolean equals = user.equals(userSecond);
+//            if (!equals){
+//                System.out.printf("НЕ РАВНЫ");
+//                System.out.printf("Hashcode %s \n", user.hashCode());
+//
+//                System.out.printf("Hashcode %s \n", userSecond.hashCode());
+//            }
+//        }
+
+
+        Long to = System.currentTimeMillis();
+        System.out.println(to - from);
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class User {
+import java.util.Objects;
+
+public class User {
+    private String name;
+    private Integer age;
+    private Integer salary;
+
+    public User(String name, Integer age, Integer salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null){
+            return false;
+        }
+
+        if (this.hashCode() == obj.hashCode())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17 ;
+        result += 31 * age;
+        result += 31 * salary;
+        result += 31 * Objects.hashCode(name);
+
+        return result;
+    }
 }
